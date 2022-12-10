73e2b55e3aad87f423d33792c7093dde619e729f
paulschuetz <paulschuetz.mail@gmail.com>
2022-12-10 07:55:40 +0100

    day10

--- src/main/kotlin/day10.kt
+++ src/main/kotlin/day10.kt
@@ -22,14 +22,10 @@ sealed interface Operation {
 }
 
 private fun solvePart1(input: List<String>): Int {
-    var registerNum = 1
-
     val ticks = input.map { parseOperation(line = it.trim()) }
-        .flatMap { op ->
-            processOperation(op = op, registerNum = registerNum).also {
-                registerNum = it.second
-            }.first
-        }
+        .scan(Pair(emptyList<Int>(), 1)) { prevOpRes, op ->
+            processOperation(op = op, registerNum = prevOpRes.second)
+        }.flatMap { it.first }
 
     val relevantCyclesNum = (ticks.size - 20).floorDiv(40) + 1
     return (0 until relevantCyclesNum)
@@ -38,23 +34,19 @@ private fun solvePart1(input: List<String>): Int {
 }
 
 private fun solvePart2(input: List<String>) {
-    var spritePos = 1
-
     input.map { parseOperation(line = it.trim()) }
-        .flatMap { op ->
-            val (ticks, newRegisterNum) = processOperation(op = op, registerNum = spritePos)
-            spritePos = newRegisterNum
-            ticks
-        }.mapIndexed { index, spritePos ->
-            // draw if circle nr (where CRT is drawing) is contained by sprite
-            if(listOf(spritePos - 1, spritePos, spritePos + 1).contains(index % 40)) "#" else ' '
+        .scan(Pair(emptyList<Int>(), 1)) { prevOpRes, op ->
+            processOperation(op = op, registerNum = prevOpRes.second)
+        }.flatMap { it.first }
+        .mapIndexed { index, spritePos ->
+            if (listOf(spritePos - 1, spritePos, spritePos + 1).contains(index % 40)) "#" else ' '
         }.chunked(40).forEach {
             println(it.joinToString(" "))
         }
 }
 
-private fun processOperation(op: Operation, registerNum: Int) : Pair<List<Int>, Int> {
-    return when(op){
+private fun processOperation(op: Operation, registerNum: Int): Pair<List<Int>, Int> {
+    return when (op) {
         is Operation.MoveSpriteOp -> {
             val ticks = listOf(registerNum, registerNum)
             return Pair(ticks, registerNum + op.offset)
@@ -64,7 +56,7 @@ private fun processOperation(op: Operation, registerNum: Int) : Pair<List<Int>, 
 }
 
 private fun parseOperation(line: String): Operation {
-    return when(line){
+    return when (line) {
         "noop" -> Operation.Noop
         else -> Operation.MoveSpriteOp(line.split(" ")[1].toInt())
     }
