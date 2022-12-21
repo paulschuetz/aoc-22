720df81dce2e66947db70d1f63034b069dc241a8
paulschuetz <paulschuetz.mail@gmail.com>
2022-12-21 22:27:30 +0100

    day 18 p2

--- src/main/kotlin/day18.kt
+++ src/main/kotlin/day18.kt
@@ -13,8 +13,8 @@ fun main() {
     val inputLines = readInputLines("day$day")
     benchmark { solvePart1(inputLines).also { println("Solution part one: $it") } }
 
-//     assertEquals(solvePart2(demoInputLines), demoInputLines.size)
-//     benchmark { solvePart2(inputLines).also { println("Solution part two: $it") } }
+    assertEquals(solvePart2(demoInputLines), 58)
+    benchmark { solvePart2(inputLines).also { println("Solution part two: $it") } }
 }
 
 data class CubePosition(val x: Int, val y: Int, val z: Int)
@@ -46,8 +46,83 @@ private fun solvePart1(input: List<String>): Int {
     return openCubeSides.values.sum()
 }
 
-val parseCube = { line: String -> line.split(",").map { it.toInt() }.let { CubePosition(it[0], it[1], it[2]) } }
-
 private fun solvePart2(input: List<String>): Int {
-    return input.size
+    val cubes = input.map { parseCube(it) }.toSet()
+
+    val minX = cubes.minOf { it.x } - 1
+    val maxX = cubes.maxOf { it.x } + 1
+    val minY = cubes.minOf { it.y } - 1
+    val maxY = cubes.maxOf { it.y } + 1
+    val minZ = cubes.minOf { it.z } - 1
+    val maxZ = cubes.maxOf { it.z } + 1
+
+    val start = CubePosition(minX, minY, minZ)
+    val surfaceCubes = mutableMapOf<CubePosition, Int>()
+    val visited = mutableSetOf(start)
+    val queue = ArrayDeque(listOf(start))
+
+    // dfs flood fill
+    while (queue.isNotEmpty()) {
+        // check top, left, right, down, front, back if within bounds
+        // If it is surface, add to surface else push onto stack
+        val currentPosition = queue.removeLast()
+
+        val right = currentPosition.copy(x = currentPosition.x + 1)
+        if (right.x in minX..maxX && right !in visited) {
+            if (right in cubes) surfaceCubes[right] = (surfaceCubes[right] ?: 0) + 1
+            else {
+                visited.add(right)
+                queue.addLast(right)
+            }
+        }
+
+        val left = currentPosition.copy(x = currentPosition.x - 1)
+        if (left.x in minX..maxX && left !in visited) {
+            if (left in cubes) surfaceCubes[left] = (surfaceCubes[left] ?: 0) + 1
+            else {
+                visited.add(left)
+                queue.addLast(left)
+            }
+        }
+
+        val top = currentPosition.copy(y = currentPosition.y - 1)
+        if (top.y in minY..maxY && top !in visited) {
+            if (top in cubes) surfaceCubes[top] = (surfaceCubes[top] ?: 0) + 1
+            else {
+                visited.add(top)
+                queue.addLast(top)
+            }
+        }
+
+        val down = currentPosition.copy(y = currentPosition.y + 1)
+        if (down.y in minY..maxY && down !in visited) {
+            if (down in cubes) surfaceCubes[down] = (surfaceCubes[down] ?: 0) + 1
+            else {
+                visited.add(down)
+                queue.addLast(down)
+            }
+        }
+
+        val back = currentPosition.copy(z = currentPosition.z - 1)
+        if (back.z in minZ..maxZ && back !in visited) {
+            if (back in cubes) surfaceCubes[back] = (surfaceCubes[back] ?: 0) + 1
+            else {
+                visited.add(back)
+                queue.addLast(back)
+            }
+        }
+
+        val front = currentPosition.copy(z = currentPosition.z + 1)
+        if (front.z in minZ..maxZ && front !in visited) {
+            if (front in cubes) surfaceCubes[front] = (surfaceCubes[front] ?: 0) + 1
+            else {
+                visited.add(front)
+                queue.addLast(front)
+            }
+        }
+    }
+
+    return surfaceCubes.values.sum()
 }
+
+val parseCube = { line: String -> line.split(",").map { it.toInt() }.let { CubePosition(it[0], it[1], it[2]) } }
